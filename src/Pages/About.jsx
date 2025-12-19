ef6a9ec6e7c4dba99e1cd2f465fcf2cf530d992c
projectswebworldhub013 <projectswebworldhub13@gmail.com>
2025-12-19 21:19:23 +0530

    another commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutHero from '../Components/AboutHero'
 import About2 from '../Components/About2'
+import HowWeWork from '../Components/HowWeWork'
 
 const About = () => {
   return (
     <div>
       <AboutHero/>
       <About2/>
+      <HowWeWork/>
     </div>
   )
 }
